619769576e962ebb43f0e6a83cb1c5169e388389
Nirmalkumar Selvaraj <Nirmalkumar.Selvaraj@lennoxintl.com>
2019-11-22 18:48:33 +0530

    Initial project setup of simple project

---
+++ sample/sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sample
+//
+//  Created by Nirmalkumar Selvaraj on 22/11/19.
+//  Copyright © 2019 Nirmalkumar Selvaraj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
